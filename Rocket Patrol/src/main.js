aff2b9df3c565cde3937921ced0c15bacd9a832d
Angela Ku <angku88@gmail.com>
2023-10-15 21:18:26 -0700

    Professor Altice tells me how to live my life

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -1,7 +1,15 @@
 let config = {
-    type: Phaser.CANVAS,
-    widht: 640,
-    height: 480
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
 }
 
+// Reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
 let game = new Phaser.Game(config);
+
+// Set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
